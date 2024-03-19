770ccce80ea8c716e29795da4e915b982bfaf5e1
Torbjörn <torbjorn.rodstrom@gmail.com>
2024-03-19 20:33:31 +0100

    Dashboard clock outline

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+//clock js
+let time = document.getElementById("clock");
+
+setInterval(() =>{
+    let t = new Date();
+    let options = { hour: '2-digit', minute: '2-digit' };
+    time.innerHTML = t.toLocaleTimeString([], options);
+},1000) // note: Initial display will take 1000 milliseconds to display.
