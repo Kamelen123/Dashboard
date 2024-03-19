97c9edda6f98ee56c22c06f1b7b0223b05f4c026
Torbjörn <torbjorn.rodstrom@gmail.com>
2024-03-19 23:46:53 +0100

    background image and bad looking button :p

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,3 +6,9 @@ setInterval(() =>{
     let options = { hour: '2-digit', minute: '2-digit' };
     time.innerHTML = t.toLocaleTimeString([], options);
 },1000) // note: Initial display will take 1000 milliseconds to display.
+//clock js
+
+//refresh background image
+function refresh(){
+    window.location.reload();
+}
